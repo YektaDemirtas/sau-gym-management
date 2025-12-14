ba4d234ed8e8d04bf0b468e4d4a94bcc4b9f7f5a
Yekta Demirtas <yekta.demirtas.1@gmail.com>
2025-12-14 22:37:03 +0300

    Preparation for db

---
+++ GymManagement/Data/DbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using GymManagement.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymManagement.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roles = { "Admin", "Member" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            string adminEmail = "ogrencinumarasi@sakarya.edu.tr";
+            string adminPassword = "sau";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if (adminUser == null)
+            {
+                adminUser = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true,
+                    FullName = "Admin User"
+                };
+
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
